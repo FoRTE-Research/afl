518e46e6a4e0406f556a87c65946c8a7196a4407
Matthew Hicks <mdhicks@gmail.com>
2018-06-11 11:41:53 -0400

    Update eval scripts for arbitrary benchmarks.

--- cluster_eval/youEdit.sh
+++ cluster_eval/youEdit.sh
@@ -1,8 +1,8 @@
 export FSF_BENCH=bsdtar
 export FSF_BENCH_PATH=/home/fuzz/Desktop/fuzzing-benchmarks/libarchive/libarchive-3.3.2
-export FSF_BENCH_ARGS=-O -xf @@
+export FSF_BENCH_ARGS=-O\ -xf\ @@
 export FSF_NUM_INPUTS=709480
-export FSF_INPUTs_PATH=/media/sf_hugeData/$FSF_BENCH
+export FSF_INPUTS_PATH=/media/sf_hugeData/$FSF_BENCH
 export FSF_EVAL_DIR=/home/mdhicks2/Desktop/afl/cluster_eval
 export FSF_AFL_DYNINST=/home/mdhicks2/Desktop/afl-dyninst
 export FSF_BASELINE_DYNINST=/home/mdhicks2/Desktop/UnTracer
