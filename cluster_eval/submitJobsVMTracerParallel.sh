3affb5ba36298eb1f7e3d45bef1f96edeaecea6d
Matthew Hicks <mdhicks@gmail.com>
2018-07-06 14:39:30 -0400

    Update eval scripts for final paper eval.

--- cluster_eval/submitJobsVMTracerParallel.sh
+++ cluster_eval/submitJobsVMTracerParallel.sh
@@ -2,24 +2,28 @@
 
 home=`pwd`
 
-for day in `seq 1 5`
+for trial in `seq 1 11`
 do
-    export FSF_DAY=$day
-    export FSF_INPUTS_PATH=/media/sf_bigdata/day${FSF_DAY}
-    export PBS_JOBID=1
-
-    for bench in bsdtar cert-basic cjson djpeg pdftohtml readelf sfconvert tcpdump
+    export PBS_JOBID=$trial
+    
+    for day in `seq 1 1`
     do
+	export FSF_DAY=$day
+	export FSF_INPUTS_PATH=/media/sf_bigdata/day${FSF_DAY}
+    
+	for bench in bsdtar cert-basic cjson djpeg pdftohtml readelf sfconvert tcpdump
+	do
 	
-	cat ${bench}.sh > benchmark.sh
-	cat ${bench}.sh > ../../UnTracer/benchmark.sh
+	    cat ${bench}.sh > benchmark.sh
+	    cat ${bench}.sh > ../../UnTracer/benchmark.sh
 
-	sh fuzzConBaseline.pbs
-	sh fuzzConDyninst.pbs
-	sh fuzzConWB.pbs
-	sh fuzzConQemu.pbs
-	#cd ../../UnTracer
-	#sh fuzzConUntracer.pbs
-	#cd $home
+	    sh fuzzConBaseline.pbs
+	    sh fuzzConDyninst.pbs
+	    sh fuzzConWB.pbs
+	    sh fuzzConQemu.pbs
+	    cd ../../UnTracer
+	    sh fuzzConUntracer.pbs
+	    cd $home
+	done
     done
 done
